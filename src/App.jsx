2551b1c5ba9a9dcd0b29d405b55aa2790a4c2481
kartikpandey0007 <kartikkpandey321@gmail.com>
2025-08-15 20:19:56 +0530

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,23 @@
 // App.js
 import React from "react";
 import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
-
 import NavBar from "./NavBar";
 import Login from "./Login";
 import Profile from "./Profile";
 import Footer from "./Footer";
+import { Provider } from "react-redux";
+import appStore from "./utils/appStore";
 
 const AppLayout = () => {
   return (
+    <Provider store={appStore}>
     <div>
       <NavBar />
       {/* Outlet renders the matching child route */}
       <Outlet />
       <Footer/>
     </div>
+    </Provider>
   );
 };
 
--- src/Login.jsx
+++ src/Login.jsx
@@ -1,11 +1,65 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
 
 const Login = () => {
-  return (
-    <div>
-      <h1> HiLogin</h1>
-    </div>
-  )
-}
+  const [emailId, setEmailId] = useState("ramlal@gmail.com");
+  const [password, setPassword] = useState("Ramlal@234");
 
-export default Login
+  const handleLogin = async () => {
+    try{
+      const res = await axios.post("http://localhost:3000/login", {
+      emailID: emailId,
+      password: password
+    },
+    {withCredentials: true}
+  )
+  }
+    catch(err){
+      console.error(err)
+    }
+
+  };//make api call to login 
+
+  return (
+    <div className="flex justify-center my-10">
+      <div className="card bg-base-300 w-96 shadow-sm">
+        <div className="card-body">
+          <h2 className="card-title">Login</h2>
+          <div>
+            <fieldset className="fieldset">
+              <legend className="fieldset-legend">Email ID</legend>
+              <input
+                type="text"
+                value={emailId}
+                className="input"
+                placeholder="abc@gmail.com"
+                onChange={(e) => setEmailId(e.target.value)}
+              />
+              <p className="label">Required*</p>
+            </fieldset>
+
+            <fieldset className="fieldset">
+              <legend className="fieldset-legend">Password</legend>
+              <input
+                type="text"
+                value={password}
+                className="input"
+                placeholder="Enter Password to login"
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              <p className="label">Required*</p>
+            </fieldset>
+          </div>
+          <div className="card-actions justify-center">
+            <button className="btn btn-primary" onClick={handleLogin}>
+              Login
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
