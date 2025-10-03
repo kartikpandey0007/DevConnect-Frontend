10c13065f138acbcbbf757e11095f424a1902fd3
kartikpandey0007 <kartikkpandey321@gmail.com>
2025-10-03 15:41:59 +0530

    fix1

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -3,9 +3,9 @@ import { BASE_URL } from "./constants"
 
 export const createSocketConnection = () => {
     if(location.hostname === "localhost") {
-        return io(BASE_URL);
+        return io(BASE_URL,{ withCredentials: true });
     }else{//on production
-        return io("/",{path:"/api/socket.io"});
+        return io("/",{path:"/api/socket.io",withCredentials: true });
     }
    
 }
